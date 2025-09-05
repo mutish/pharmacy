4aee4e9944825c072df24d208e6181c5546c7179
mutish <mutindastacey@gmail.com>
2025-09-05 12:12:48 +0300

    Completed Authentication and Roles

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,17 @@
- import express from "express";
- import cors from "cors";
- import dotenv from "dotenv";
- import helmet from "helmet";
- import morgan from "morgan";
- import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
- dotenv.config();
+import authRoutes from "./routes/auth.routes.js";
 
- const app = express();
- 
+
+dotenv.config();
+
+const app = express();
+
 //middleware
 app.use(helmet());   // Security headers
 app.use(cors({ origin: "*", credentials: true }));  // Cross-origin support
@@ -20,24 +23,23 @@ app.use(cookieParser());  //handle cookies
 //health route
 
 app.get("/api/health", (req, res) => {
-    res.json({status: "ok", message: "Backend is running"});
+   res.json({status: "ok", message: "Backend is running"});
 });
 
 //placeholder routes
-// import authRoutes from "./routes/auth.routes.js";
-// import productRoutes from "./routes/product.routes.js";
+
 // import orderRoutes from "./routes/order.routes.js";
 // import paymentRoutes from "./routes/payment.routes.js";
 
-// app.use("/api/auth", authRoutes);
-// app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
+
 // app.use("/api/orders", orderRoutes);
 // app.use("/api/payments", paymentRoutes);
 
 //Global error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({message: "Something went wrong on the server"});
+   console.error(err.stack);
+   res.status(500).json({message: "Something went wrong on the server"});
 });
 
 export default app;
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { register, login, logout } from "../controllers/auth.controller.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import roleMiddleware from "../middlewares/roleMiddleware.js";
+
+const router = express.Router();
+// Public
+router.post("/register", register);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+// Example: protected route
+router.get("/admin-dashboard", authMiddleware, roleMiddleware("admin"), (req, res) => {
+  res.json({ message: "Welcome Admin 🚀" });
+});
+
+
+export default router;
